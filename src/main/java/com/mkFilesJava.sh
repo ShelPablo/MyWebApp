3ccd582a073343dea42782ac4df7cb166b1b92c7
shel <shel-ne@mail.ru>
2018-10-12 19:32:28 +0300

    init commit +index Homepage at users/index

---
+++ src/main/java/com/mkFilesJava.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+touch ../../resources/application.properties
+
+touch MyApplication.java
+cd concretepage/controller
+touch ArticleController.java
+cd ../../concretepage/dao
+touch ArticleDAO.java IArticleDAO.java
+cd ../../concretepage/entity
+touch Article.java
+cd ../../concretepage/service
+touch ArticleService.java IArticleService.java
+
+
+   
---
+++ target/classes/mkDirStructure.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+mkdir -R com/concretepage
+cd com/concretepage
+mkdir controller dao entity service 
